2d0fb543950e4488b918db4a94d4f270a0842bff
khalid hassan <abuuanas30@hotmail.com>
2017-11-03 06:58:23 +0300

    fixing bugs

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,38 +1,38 @@
-from flask import render_template,redirect,url_for,request
-from flask_login import login_required
-from .. import db
 from . import auth
-from flask_login import login_user
-from .forms import LoginForm
-from .forms import RegistrationForm
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
 from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login', methods = ["GET", "POST"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
+            login_user(user, login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
-        flash('Invalid username or Password')
+        flash('Invalid username or password')
 
-    title = "oneminutepitch login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+    title = "Login"
+    return render_template('auth/login.html', title = title, login_form = login_form)
 
-from flask_login import login_user,logout_user,login_required
+@auth.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.login'))
+        flash('Your account was registered successfully. You can now log in.')
+
+    title = "New Account"
+    return render_template('auth/register.html', title = title, registration_form = form)
+
 @auth.route('/logout')
 @login_required
 def logout():
--- manage.py
+++ manage.py
@@ -1,19 +1,27 @@
 from app import create_app, db
-from flask_script import Manager,Server
-from flask_migrate import Migrate,MigrateCommand
-from app.models import User, Pitch
+from flask_script import Manager, Server
+from app.models import User,  Category, Pitch 
+from flask_migrate import Migrate, MigrateCommand
+
+# create app instance
+app =  create_app('production')
 
 
-app = create_app('development')
 manager = Manager(app)
-manager.add_command('server',Server)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+manager.add_command('server', Server)
+@manager.command
+def test():
+    '''run utitest'''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app, db = db, User = User, Category = Category, Pitch = Pitch, Feedback = Feedback)
 
-
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
